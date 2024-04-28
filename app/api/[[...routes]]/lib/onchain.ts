1ca0450bfafb20801f435d2dc06a861ea3a1c036
Saeed <hamidradpj@gmail.com>
2024-04-28 16:20:31 +0300

    Add MAX_FEE_PER_GAS

--- app/api/[[...routes]]/lib/onchain.ts
+++ app/api/[[...routes]]/lib/onchain.ts
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
-import { createWalletClient, http, getContract, parseEther } from "viem";
+import {
+  parseUnits,
+  createWalletClient,
+  http,
+  getContract,
+  parseEther,
+  encodeFunctionData,
+} from "viem";
 import { base, baseSepolia } from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
 import type { Address } from "viem";
 import { abi } from "./abi";
+import { env } from "process";
+import { log } from "./functions";
 
 dotenv.config();
 
@@ -22,20 +31,26 @@ const client = createWalletClient({
   ),
 });
 
-const token = getContract({
-  address:
-    isTesting === "true"
-      ? (process.env.TEST_TOKEN_ADDRESS as `0x${string}`)
-      : (process.env.TOKEN_ADDRESS as `0x${string}`),
-  abi,
-  client,
-});
-
 export const transferToken = async function (
   wallet: Address
 ): Promise<`0x${string}`> {
-  return await token.write.transfer([
-    wallet,
-    parseEther(process.env.ALLOCATION_AMOUNT as string),
-  ]);
+  const tokenAddress =
+    isTesting === "true"
+      ? (process.env.TEST_TOKEN_ADDRESS as `0x${string}`)
+      : (process.env.TOKEN_ADDRESS as `0x${string}`);
+
+  const data = encodeFunctionData({
+    abi,
+    functionName: "transfer",
+    args: [wallet, parseEther(process.env.ALLOCATION_AMOUNT as string)],
+  });
+
+  const txHash = await client.sendTransaction({
+    account: vault,
+    to: tokenAddress,
+    data: data,
+    maxFeePerGas: parseUnits(process.env.MAX_FEE_PER_GAS as string, 9),
+  });
+  log(`Wallet address is ${wallet}, tx hash is ${txHash}`);
+  return txHash;
 };
